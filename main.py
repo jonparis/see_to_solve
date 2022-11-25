bda05aebb2247e3799ea69aa604df40662e34d3e
Jon <jonparis@gmail.com>
2022-11-25 15:05:35 -0800

    updated to annotate move more clearly.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
 import torch
+import numpy
+import string
 import time
 import os
 from board_detector.chessposition import Utils
@@ -8,11 +10,42 @@ from stockfish.stockfish import Stockfish, StockfishException
 stockfish = Stockfish()
 
 
-class SeeToSolve:
-    def __init__(self):
+class SeeToSolve():
+    def __init__(self, playing):
+        if playing == "" or playing != "b":
+            self.playing = "w"
+        else:
+            self.playing = "b"
         self.image_path = None
         self.model_path = "board_detector/models/position_predict.pt"
         self.my_model = torch.jit.load(self.model_path)
+        self.rename = True
+
+    @staticmethod
+    def fen_to_positions(fen):
+        label_array = []
+        for i in fen:
+            if str.isdigit(i):
+                label_array += numpy.zeros(int(i), numpy.int16).tolist()
+            elif i != "-":
+                label_array.append(i)
+        return label_array
+
+    @staticmethod
+    def annotated_move(fen, move):
+        position_array = SeeToSolve.fen_to_positions(fen)
+        start_position = string.ascii_lowercase.index(move[0]) + 8 * (8 - int(move[1]))
+        end_position = string.ascii_lowercase.index(move[2]) + 8 * (8 - int(move[3]))
+
+        piece_names = {"p": "Pawn", "b": "Bishop", "n": "Knight", "r": "Rook", "k": "King", "q": "Queen"}
+
+        moving_piece_desc = piece_names[position_array[start_position].lower()]
+
+        if not str.isdigit(str(position_array[end_position])):
+            end_piece_desc = piece_names[position_array[end_position].lower()]
+            print("Move", moving_piece_desc, "on", move[0:2], "to take", end_piece_desc, "on", move[2:4])
+        else:
+            print("Move", moving_piece_desc, "on", move[0:2], "to", move[2:4])
 
     @staticmethod
     def get_last_image(dirpath, image_ext=('jpg','jpeg','png')):
@@ -23,6 +56,8 @@ class SeeToSolve:
 
     def recommend_move(self):
         fen = Utils.pred_single_img(self.model_path, self.image_path)
+        if self.playing == "b":
+            fen = fen[::-1]
         print(fen)
         new_fen = fen.replace("-", "/")
         white_fen = new_fen.strip() + " w"
@@ -45,8 +80,16 @@ class SeeToSolve:
         if not black_valid and not white_valid:
             print("no valid moves - checkmate?")
         else:
-            if white_valid: print("white's best move: ", whites_best_move)
-            if black_valid: print("black's best move: ", blacks_best_move)
+            if white_valid and self.playing == "w": 
+                self.annotated_move(fen, whites_best_move)
+            if black_valid and self.playing == "b": 
+                self.annotated_move(fen, blacks_best_move)
+            if self.rename:
+                original = self.image_path
+
+                new_name = os.path.dirname(self.image_path) + "/" + fen + " " +  str(time.time()) + ".png"
+                os.rename(original, new_name)
+                self.image_path = new_name
 
     def check_for_new_image(self):
         new_image = self.get_last_image(screenshot_path)
@@ -58,7 +101,8 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.withdraw()
     screenshot_path = filedialog.askdirectory()
-    see_to_solve = SeeToSolve()
+    playing = input("are you playing White (w) or Black (b)?").lower()
+    see_to_solve = SeeToSolve(playing)
     starttime = time.time()
     interval = 1.0 # seconds
     while True:
