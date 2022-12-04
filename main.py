9e46bd534405e80974fc4a49093f8b1cd501cd7e
Jon <jonparis@gmail.com>
2022-12-04 08:24:43 -0800

    increased usability
    1. auto detect whether playing black or white and
    2. save screenshot directory to an ini fiel

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ import time
 import os
 from board_detector.chessposition import Utils, CONST
 from stockfish.stockfish import Stockfish, StockfishException
+from configparser import ConfigParser
 stockfish = Stockfish()
 
 class SeeToSolve():
-    def __init__(self, playing):
-        if playing != "b":
-            self.playing = "w"
-        else:
-            self.playing = "b"
-        print(self.playing)
+    def __init__(self):
+        self.screenshots_path = self.get_screenshots_path()
         self.image_path = None
         self.model_path = "board_detector/models/position_predict_stable.pt"
         self.my_model = torch.jit.load(self.model_path)
         self.rename = True
+        self.playing = None
 
     @staticmethod
     def fen_to_positions(fen):
@@ -47,30 +45,72 @@ class SeeToSolve():
         else:
             print("Move", moving_piece_desc, "on", move[0:2], "to", move[2:4])
 
-    @staticmethod
-    def get_last_image(dirpath, image_ext=('jpg','jpeg','png')):
-            files = [os.path.join(dirpath, filename) for filename in os.listdir(dirpath)]
+    
+    def get_screenshots_path(self):
+        config = ConfigParser()
+
+        if os.path.exists("config.ini"):
+            config.read("config.ini")
+            try:
+                screenshots_path = config["DEFAULTS"]["screenshots_path"]
+            except:
+                print("Please select the folder where screenshots are saved")
+                screenshots_path = filedialog.askdirectory(title="Please select the folder where screenshots are saved:")
+                self.save_screenshots_path(screenshots_path)
+        else:
+            print("Please select the folder where screenshots are saved")
+            screenshots_path = filedialog.askdirectory(title="Please select the folder where screenshots are saved:")
+            self.save_screenshots_path(screenshots_path)
+
+        return screenshots_path
+   
+    def save_screenshots_path(self, screenshots_path):
+        config = ConfigParser()
+        config["DEFAULTS"] = {"screenshots_path": screenshots_path}
+        with open('config.ini', 'w') as conf:
+            config.write(conf)
+
+    def get_last_image(self, image_ext=('jpg','jpeg','png')):
+            files = [os.path.join(self.screenshots_path, filename) for filename in os.listdir(self.screenshots_path)]
             valid = [f for f in files if '.' in f and f.rsplit('.',1)[-1] in image_ext and os.path.isfile(f)]
             if valid:
                 return max(valid, key=os.path.getmtime)
 
+    def set_playing(self, fen):
+        if self.playing != None: 
+            return
+        playing_black = Utils.playing_black(fen)
+        if playing_black == True: 
+            self.playing = "b"
+        elif playing_black == False:
+            self.playing = "w"
+        else:
+            playing = input("are you playing White (w) or Black (b)?").lower()
+            if playing != "b":
+                self.playing = "w"
+            else:
+                self.playing = "b"
+        print(self.playing)
+
+
+
+
+
     def recommend_move(self):
         pred_fen = fen = Utils.pred_single_img(self.model_path, self.image_path)
+        self.set_playing(pred_fen)
         if self.playing == "b":
             fen = fen[::-1]
         new_fen = fen.replace("-", "/")  + " " + self.playing
         best_move = False
-        
-        try:
-            is_valid = stockfish.is_fen_valid(new_fen)
-        except StockfishException:
-            print("crash checking if fen is valid")
-
+        is_valid = True        
         
         try:
             stockfish.set_fen_position(new_fen)
         except StockfishException:
+            is_valid = False
             print("crash when setting fen")
+        
         if is_valid:
             try:
                 best_move = stockfish.get_best_move()
@@ -79,7 +119,7 @@ class SeeToSolve():
                 best_move = False
         else:
             print("not a valid fen:", new_fen)
-            
+
         try:
             if self.playing == "b": 
                 print(stockfish.get_board_visual(False))
@@ -101,19 +141,18 @@ class SeeToSolve():
             self.image_path = new_name
 
     def check_for_new_image(self):
-        new_image = self.get_last_image(screenshot_path)
-        if self.image_path is None or new_image != self.image_path:
+        new_image = self.get_last_image()
+        if self.image_path is None or new_image.split()[0] != self.image_path.split()[0]:
             self.image_path = new_image
             self.recommend_move()
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.withdraw()
-    screenshot_path = filedialog.askdirectory()
-    playing = input("are you playing White (w) or Black (b)?").lower()
-    see_to_solve = SeeToSolve(playing)
+    see_to_solve = SeeToSolve()
     starttime = time.time()
     interval = 0.5 # seconds
     while True:
         see_to_solve.check_for_new_image()
         time.sleep(interval - ((time.time() - starttime) % interval))
+
