1dc7786ce6d7d76ae99f72df2fc12619e9652089
Jon <jonparis@gmail.com>
2022-12-18 23:27:26 -0800

    tap for a single image

--- see_to_solve_chrome_ext/background.js
+++ see_to_solve_chrome_ext/background.js
@@ -1,26 +1,34 @@
-if (typeof get_images === 'undefined'){
-    var get_images = false;
-}
-
+var continuous_capture = false  // Set to true for continuous capture
+var capture_every_x_seconds = 5;
 chrome.action.onClicked.addListener(function (tab) {
-    if(get_images){
-        clearInterval(get_images);
-        get_images = false;
-    }
-    else {
-        get_images = setInterval(function() { 
-            chrome.tabs.captureVisibleTab(tab.windowId, { format: "png" }, function(dataUrl) {
-                setTimeout(() => {
-                        chrome.tabs.sendMessage(tab.id, {name: "capture", dataUrl: dataUrl}, 
-                            (response) => {
-                                if (response.success) {
-                                    console.log("success")
-                                } else {
-                                    console.log("failed download")
-                                }
-                            })
-                    }, 200);
-            });
-        }, 5000);
+    if(continuous_capture){
+        if (typeof get_images === 'undefined'){
+            var get_images = false;
+        }
+        if(get_images){
+            clearInterval(get_images);
+            get_images = false;
+        }
+        else {
+            get_images = setInterval(function() { capture_board(tab); }, 1000 * capture_every_x_seconds);
+        }
+    } else {
+        capture_board(tab);
     }
 });
+
+
+function capture_board(tab){
+    chrome.tabs.captureVisibleTab(tab.windowId, { format: "png" }, function(dataUrl) {
+        setTimeout(() => {
+                chrome.tabs.sendMessage(tab.id, {name: "capture", dataUrl: dataUrl}, 
+                    (response) => {
+                        if (response.success) {
+                            console.log("success")
+                        } else {
+                            console.log("failed download")
+                        }
+                    })
+            }, 200);
+    });
+}
